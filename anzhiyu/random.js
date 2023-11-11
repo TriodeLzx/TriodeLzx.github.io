43c1d5358206c22d86bf3f39a760c6204f8e48f2
TriodeLzx <Lzx0916l@163.com>
2023-11-11 21:12:59 +0800

    Site updated: 2023-11-11 21:12:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/06/Triode域/","2023/11/11/深大Aurora第一批入队赛WP/"];function toRandomPost(){
+var posts=["2023/11/06/Triode域/","2023/11/11/深大Aurora第一批入队赛WP/","2023/11/07/TEST-PAGE/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
