77fc71adb29d40395e78a6c90aa80ce86291554b
TriodeLzx <Lzx0916l@163.com>
2023-12-15 21:00:10 +0800

    Site updated: 2023-12-15 21:00:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/11/深大Aurora第一批入队赛WP/","2023/11/17/Triode的RSA学习笔记（1）/"];function toRandomPost(){
+var posts=["2023/11/11/深大Aurora第一批入队赛WP/","2023/11/17/Triode的RSA学习笔记（1）/","2023/12/15/Anshel–Anshel–Goldfeld-密钥交换体系-Anshel–Anshel–Goldfeld-key-exchange/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
